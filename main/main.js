ce16608a62f415d009ad312c88eb83fcd64f5d18
root <root@vm-10-112-43-106.(none)>
2018-01-11 11:59:08 +0800

    加广告词

--- main/main.js
+++ main/main.js
@@ -4,18 +4,24 @@ const app = require('koa')();
 const router = require('koa-router')();
 
 router.get('/lempb', function *(next){
-    var t = this.query && this.query.t || '';
+    var query = this.query || {};
+    var t = query && query.t || '';
     try{
-        var pb = this.query && decodeURIComponent(this.query.pb) || '';
-        if(pb){
-                var data = t + ':' + pb;
-                console.info(data);
-        }
+    	var pb = query && decodeURIComponent(query.pb) || '';
+	var d = query && query.d || '';
+    	if(pb){
+		var data = t + ':' + pb;
+		if(d && d != undefined && d != '' && d != null){
+		    data = data + '&&&' + decodeURIComponent(d);
+		}
+       	 	console.info(data);
+    	}
 
-        this.body = 'ok';
+   	this.body = 'ok';
     }catch (e){}
 });
 
 app.use(router.routes());
 
 app.listen(10086);
+
--- util/formatTime.js
+++ util/formatTime.js
@@ -22,3 +22,4 @@ module.exports = function (time, timeFormat) {
                         return t[$0] || '';
                 });
         };
+
